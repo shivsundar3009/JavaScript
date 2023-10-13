2839514f2d1756ce757c7b43ddd144784bfc756c
shivsundar wakle <126707206+shivsundar3009@users.noreply.github.com>
2023-10-13 23:52:10 +0530

    Updated notes

---
+++ 01_js_getting_started.js
@@ -0,0 +1,16 @@
+// Javascript is a lightweight interpreted programming language with first class functions. It is well known as the scripting language for web pages.
+
+// Javascript was invented by Brenden Eich in 1995. It was developed for Netscape 2 and became the ECMA - 262 standard in 1997.
+
+// Browser environment also use it such as Node.js, Apache and Deno.
+
+// Node.js was written intially by Ryan Dahl in 2009. The initial release supported only Linux and Mac.
+
+console.log("Note: View Files from this Repl to Access the Source Code")
+console.log("This program adds 1, 2 and 54")
+
+function addThreeNumbers(a, b, c) {
+    return a + b + c;
+}
+let c = addThreeNumbers(1, 2, 54)
+console.log(c)
