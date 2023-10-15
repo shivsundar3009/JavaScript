e5c32f60c5c5e302b82840ccaf082643d6308492
shivsundar wakle <126707206+shivsundar3009@users.noreply.github.com>
2023-10-15 23:32:47 +0530

    Update 02_requiredTools.js

--- 02_requiredTools.js
+++ 02_requiredTools.js
@@ -1,8 +1,16 @@
 // In this we are installing required tools to run javascript in our machine
 
+// We need a runtime environment to run javascript in our system we are using Nodejs
+
+// Node.js was written intially by Ryan Dahl in 2009. The initial release supported only Linux and Mac.
+
+// we are also using Microsoft Visual Studio Code as a code editor
+
+// It was created by Eric Gamma and intially released in 2015
+
 var a = 67 // a contains 67
 console.log(a)
-a = "harry"
+a = "shiv"
 console.log(a)
-// let 8harry = 7 // Not allowed this will throw an error
+// let 8shiv = 7 // Not allowed this will throw an error
 // let var = 7
