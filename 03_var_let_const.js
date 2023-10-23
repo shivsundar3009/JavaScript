5904eabb2cabacf7474cb3b85acd6c1e43a099df
shivsundar3009 <shivsundar3009@gmail.com>
2023-10-23 21:05:30 +0530

    update var_let_const

--- 03_var_let_const.js
+++ 03_var_let_const.js
@@ -1,4 +1,5 @@
-/*
+/* this is a multiline comment
+
 Javascript variables can be declared inn 4 ways:
   A) Automatically 
   B) Using Var
@@ -12,3 +13,23 @@ Javascript variables can be declared inn 4 ways:
 // The Let and Const keywords were introduced to Javascript in 2015.
 
 // Only use let if you cannot use const.
+
+const accountId = 144553
+
+let accountEmail = 'shiv@google.com'
+
+var accountPassword = 'Shiv@123'
+
+accountCity = 'Indore'
+
+let accountState
+
+console.log(accountId);
+
+// console.table([accountId,accountEmail,accountPassword,accountCity])
+// we can use console.table([var1,var2----]) to print multiple variables
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+// we can also declare variable without adding value to it, it will return undefined
+
+// we have to assign value when declaring variable using const
