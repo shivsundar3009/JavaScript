7fe771e63ffd4b0df142dda61647254c860153ef
shivsundar wakle <126707206+shivsundar3009@users.noreply.github.com>
2023-10-26 22:58:49 +0530

    Update 04_dataTypes.js

--- 04_dataTypes.js
+++ 04_dataTypes.js
@@ -5,7 +5,7 @@ JavaScript has 8 Datatypes
 3. Bigint
 4. Boolean
 5. Undefined
-6. Null
+6. Null     returns type as a OBJECT
 7. Symbol
 8. Object
 
@@ -21,3 +21,16 @@ The object data type can contain:
 testing something
 
 */
+
+'use strict' // treat all js code as newer version
+
+// alert(3 +3) this works only in browsers
+
+// console.log(3 +3); console.log(shiv); // this is a bad practice
+
+let name = 'shivsundar'
+let age = '22'
+let isLoggedIn = true
+
+
+console.log(typeof null);
