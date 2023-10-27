9baeeabb9667d84a37880a6191a21f2ada4cbcdb
shivsundar3009 <shivsundar3009@gmail.com>
2023-10-27 21:14:58 +0530

    update dataTypeConversion

--- 05_dataTypeConversion.js
+++ 05_dataTypeConversion.js
@@ -1,3 +1,28 @@
-// we are learning about dataType conversion 
+let score = "shivsundar"
 
-// we can convert string to number by using parseInt() function
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "shivsundar"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
