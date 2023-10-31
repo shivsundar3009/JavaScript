db0cc2d6b424fdbc4aef84f4c96759b495505ac0
shivsundar wakle <126707206+shivsundar3009@users.noreply.github.com>
2023-10-31 19:48:11 +0530

    string methods

--- 07_strings.js
+++ 07_strings.js
@@ -4,3 +4,16 @@ const repoCount = 50
 // console.log(name + repoCount + " Value");
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// STRING methods
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
