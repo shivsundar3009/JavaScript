74ca251ced2763a14e17f2815bbca4c68bcd71a5
shivsundar wakle <126707206+shivsundar3009@users.noreply.github.com>
2023-11-04 20:13:20 +0530

    Create 09_dates.js

---
+++ 09_dates.js
@@ -0,0 +1,7 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
