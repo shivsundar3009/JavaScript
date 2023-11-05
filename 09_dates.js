8f9f93dd03a51f3fd33a988d8c3359617e5e188f
shivsundar wakle <126707206+shivsundar3009@users.noreply.github.com>
2023-11-05 21:21:27 +0530

    Update 09_dates.js

--- 09_dates.js
+++ 09_dates.js
@@ -5,3 +5,9 @@ let myDate = new Date()
 // console.log(myDate.toDateString());
 // console.log(myDate.toLocaleString());
 // console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
