c1bac8cfd3fd54f6601f318b2073ab735b2a69bc
shivsundar wakle <126707206+shivsundar3009@users.noreply.github.com>
2023-11-09 22:39:03 +0530

    Update 10_array.js

--- 10_array.js
+++ 10_array.js
@@ -5,3 +5,21 @@ const myHeors = ["shaktiman", "naagraj"]
 
 const myArr2 = new Array(1, 2, 3, 4)
 // console.log(myArr[1]);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log( newArr);
+
