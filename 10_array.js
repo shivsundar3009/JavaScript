62c5129806787c4b75f9195800ad731f3cf2c4c1
shivsundar wakle <126707206+shivsundar3009@users.noreply.github.com>
2023-11-10 22:24:22 +0530

    Update 10_array.js

--- 10_array.js
+++ 10_array.js
@@ -23,3 +23,16 @@ const myArr2 = new Array(1, 2, 3, 4)
 // console.log(myArr);
 // console.log( newArr);
 
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
