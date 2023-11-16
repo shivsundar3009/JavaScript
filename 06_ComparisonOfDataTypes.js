5717c48d3fd7e593382fede6ca175a2675df2aac
shivsundar wakle <126707206+shivsundar3009@users.noreply.github.com>
2023-11-16 22:25:08 +0530

    Update 06_ComparisonOfDataTypes.js

--- 06_ComparisonOfDataTypes.js
+++ 06_ComparisonOfDataTypes.js
@@ -8,6 +8,9 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
+// The reason is that an equality check == and comparisons > < >= <= works differently
+// comparisons convert null to a number treating as 0 thats why (3) null >=0 is true and (1) null > 0 is false
+
 console.log(null > 0);
 console.log(null == 0);
 console.log(null >= 0);
