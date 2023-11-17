7342d40616895c0fbfbf5575715bde5763acd684
shivsundar wakle <126707206+shivsundar3009@users.noreply.github.com>
2023-11-17 23:50:03 +0530

    Update 11_array2.js

--- 11_array2.js
+++ 11_array2.js
@@ -17,3 +17,13 @@ const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
 const real_another_array = another_array.flat(Infinity)
 console.log(real_another_array);
+
+console.log(Array.isArray("Hitesh"))
+console.log(Array.from("Hitesh"))
+console.log(Array.from({name: "hitesh"})) // interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
