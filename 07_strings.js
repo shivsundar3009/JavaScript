15d32b32b003e0a29d9ec360b6dfae793d877c3c
shivsundar wakle <126707206+shivsundar3009@users.noreply.github.com>
2023-11-21 22:37:10 +0530

    Update 07_strings.js

--- 07_strings.js
+++ 07_strings.js
@@ -3,17 +3,28 @@ const repoCount = 50
 
 // console.log(name + repoCount + " Value");
 
+// backticks " `` " was introduced in es6 version 
+
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 // STRING methods
 
-// console.log(gameName.length);
-// console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
+// It does not change the original string
+
+
+// console.log(gameName.length); // to get the length of the string
+
+// console.log(gameName.toUpperCase()); // to convert the string to UPPER CASE
+
+console.log(gameName.charAt(2)); // to get CHAR at given Index
 console.log(gameName.indexOf('t'));
 
+// slice(indexStart, indexEnd) // last index isn't included
 const newString = gameName.substring(0, 4)
 console.log(newString);
 
+
+// slice(indexStart)
+// slice(indexStart, indexEnd)
 const anotherString = gameName.slice(-8, 4)
 console.log(anotherString);
