a3c55b7691dae9b623c3ed29e60b254c0b80229e
shivsundar wakle <126707206+shivsundar3009@users.noreply.github.com>
2023-11-22 23:13:35 +0530

    Update 08_nums_maths.js

--- 08_nums_maths.js
+++ 08_nums_maths.js
@@ -4,19 +4,30 @@ const score = 400
 const balance = new Number(100)
 // console.log(balance);
 
+// we can convert Num to String and use string Methods
 // console.log(balance.toString().length);
-// console.log(balance.toFixed(1));
+
+// let a = 500 // it changes 500 to 500.00(fixed value)
+// console.log(balance.toFixed(2));
 
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
 // console.log(Math);
-// console.log(Math.abs(-4));
-// console.log(Math.round(4.6));
-// console.log(Math.ceil(4.2));
-// console.log(Math.floor(4.9));
-// console.log(Math.min(4, 3, 6, 8));
-// console.log(Math.max(4, 3, 6, 8));
 
-console.log(Math.random());
-console.log((Math.random()*10) + 1);
+// console.log(Math.abs(-4)); // changes value to absolute
+
+// console.log(Math.round(4.6)); // roudfigures the value
+
+// console.log(Math.ceil(4.2)); // get top value it will give 5
+
+// console.log(Math.floor(4.9)); // get lower value it will give 4
+
+// console.log(Math.min(4, 3, 6, 8)); // gets minimum value
+
+// console.log(Math.max(4, 3, 6, 8)); // gets maximum value
+
+console.log(Math.random()); // generates random value between 0 to 1
+
+console.log((Math.random()*10) + 1); 
+
 console.log(Math.floor(Math.random()*10) + 1);
