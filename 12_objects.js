3fe816cde9a139aa2b49fe762c227a31ceb57549
shivsundar wakle <126707206+shivsundar3009@users.noreply.github.com>
2023-11-26 23:34:15 +0530

    Update 12_objects.js

--- 12_objects.js
+++ 12_objects.js
@@ -37,3 +37,27 @@ JsUser.greetingTwo = function(){
 
 console.log(JsUser.greeting());
 console.log(JsUser.greetingTwo());
+
+===================================================================================================================================================
+
+
+ // const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hitesh",
+            lastname: "choudhary"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
