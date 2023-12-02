cc08f3f6f4f46bc80a41a7d179f7c15461b1c24c
shivsundar wakle <126707206+shivsundar3009@users.noreply.github.com>
2023-12-02 23:32:01 +0530

    Update 15_scopes.js

--- 15_scopes.js
+++ 15_scopes.js
@@ -39,3 +39,19 @@ if (true) {
 }
 
 // console.log(username);
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
