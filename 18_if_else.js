598756ba193443d59b356bf73e553e1ee2f7542a
shivsundar wakle <126707206+shivsundar3009@users.noreply.github.com>
2023-12-12 22:15:21 +0530

    Create 18_if_else.js

---
+++ 18_if_else.js
@@ -0,0 +1,21 @@
+// if
+const isUserloggedIn = true
+const temperature = 41
+
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
