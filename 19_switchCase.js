416f8caa62f13c79234549441267efc749c5a088
shivsundar wakle <126707206+shivsundar3009@users.noreply.github.com>
2023-12-20 23:47:52 +0530

    Update 19_switchCase.js

--- 19_switchCase.js
+++ 19_switchCase.js
@@ -1,3 +1,23 @@
+/*The JavaScript Switch Statement
+Use the switch statement to select one of many code blocks to be executed. 
+
+The switch expression is evaluated once.
+The value of the expression is compared with the values of each case.
+If there is a match, the associated block of code is executed.
+If there is no match, the default code block is executed.
+
+########### The break Keyword ##########
+
+When JavaScript reaches a break keyword, it breaks out of the switch block.
+
+This will stop the execution inside the switch block.
+
+It is not necessary to break the last case in a switch block. The block breaks (ends) there anyway.
+
+Note: If you omit the break statement, the next case will be executed even if the evaluation does not match the case.
+
+
+        
 // switch (key) {
 //     case value:
         
