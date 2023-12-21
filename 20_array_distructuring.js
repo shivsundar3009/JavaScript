464b8b4506605be21eb53b872fb5e087b4acad38
shivsundar wakle <126707206+shivsundar3009@users.noreply.github.com>
2023-12-21 23:54:56 +0530

    Create 20_array_distructuring.js

---
+++ 20_array_distructuring.js
@@ -0,0 +1,15 @@
+The destructuring assignment syntax is a JavaScript expression that makes it possible to unpack values from arrays, or properties from objects, into distinct variables.
+
+  let a, b, rest;
+[a, b] = [10, 20];
+
+console.log(a);
+// Expected output: 10
+
+console.log(b);
+// Expected output: 20
+
+[a, b, ...rest] = [10, 20, 30, 40, 50];
+
+console.log(rest);
+// Expected output: Array [30, 40, 50]
