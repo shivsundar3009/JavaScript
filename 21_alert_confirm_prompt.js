1530fb2bb625deeef69206731025f161cf5d349b
shivsundar wakle <126707206+shivsundar3009@users.noreply.github.com>
2024-01-10 18:28:07 +0530

    Create 21_alert_confirm_prompt.js

---
+++ 21_alert_confirm_prompt.js
@@ -0,0 +1,19 @@
+01. window.alert() instructs the browser to display a dialog with an optional message, and to wait until the user dismisses the dialog.
+
+  `alert()
+   alert(message) `
+returns an undefined value
+
+02. window.confirm() instructs the browser to display a dialog with an optional message, and to wait until the user either confirms or cancels the dialog.
+
+  ' confirm(message) '
+returns a boolean indicating whether OK (true) or Cancel (false) was selected. 
+
+
+03. window.prompt() instructs the browser to display a dialog with an optional message prompting the user to input some text, and to wait until the user either submits the text or cancels the dialog.
+
+ /* prompt()
+  prompt(message)
+  prompt(message, defaultValue) */
+returns a string containing the text entered by the user, or null.
+
