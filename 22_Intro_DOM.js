62660de87579db113a0eabe5167a1a1c8df365bd
shivsundar wakle <126707206+shivsundar3009@users.noreply.github.com>
2024-01-17 23:55:38 +0530

    Create 22_Intro_DOM.js

---
+++ 22_Intro_DOM.js
@@ -0,0 +1,5 @@
+What is the DOM?
+  
+The Document Object Model (DOM) is a programming interface for web documents. It represents the page so that programs can change the document structure, style, and content. The DOM represents the document as nodes and objects; that way, programming languages can interact with the page.
+
+All of the properties, methods, and events available for manipulating and creating web pages are organized into objects. For example, the document object that represents the document itself, any table objects that implement the HTMLTableElement DOM interface for accessing HTML tables, and so forth, are all objects.
