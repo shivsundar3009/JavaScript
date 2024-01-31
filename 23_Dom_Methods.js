6e0ce51ba54ffcf98b50b7b6964a9b74be1744e2
shivsundar wakle <126707206+shivsundar3009@users.noreply.github.com>
2024-01-31 23:59:23 +0530

    Update 23_Dom_Methods.js

--- 23_Dom_Methods.js
+++ 23_Dom_Methods.js
@@ -14,4 +14,17 @@ A property is a value that you can get or set (like changing the content of an H
 
 A method is an action you can do (like add or deleting an HTML element).  
 
+
+The getElementById Method
+
+The most common way to access an HTML element is to use the id of the element.
+
+In the example above the getElementById method used id="demo" to find the element.
+
+The innerHTML Property
+
+The easiest way to get the content of an element is by using the innerHTML property.
+
+The innerHTML property is useful for getting or replacing the content of HTML elements. 
+  
   
