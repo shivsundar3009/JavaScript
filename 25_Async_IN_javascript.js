9ff158bfe01058709aeedeba289bcb7896b96b2f
shivsundar wakle <126707206+shivsundar3009@users.noreply.github.com>
2024-02-15 23:30:48 +0530

    Create 25_Async_IN_javascript.js

---
+++ 25_Async_IN_javascript.js
@@ -0,0 +1,17 @@
+In summary, asynchronous programming is an essential concept in JavaScript that allows your code to run in the background without blocking the execution of other code.
+  Developers can create more efficient and responsive applications by using features like callbacks, async/await, and promises.
+
+ 
+Asynchronous programming in JavaScript is a crucial concept, especially for handling tasks that might take time to complete,
+such as fetching data from an API, reading files, or executing complex calculations. JavaScript is inherently single-threaded, 
+meaning it can only execute one piece of code at a time. Asynchronous programming allows JavaScript to execute non-blocking code, 
+enabling it to perform multiple operations simultaneously without waiting for each one to finish before moving on to the next.
+
+There are several mechanisms for asynchronous programming in JavaScript: 
+
+Callbacks:
+Promises:
+Async/Await:
+
+Each of these methods has its advantages and use cases. Promises and async/await are generally preferred over callbacks due to their cleaner syntax and better error 
+handling capabilities. However, understanding callbacks is important since many older APIs still use them, and they're fundamental to understanding asynchronous programming in JavaScript.
