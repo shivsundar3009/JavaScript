688e836503e4bde4082a5817a5f9fdd07287d43c
shivsundar wakle <126707206+shivsundar3009@users.noreply.github.com>
2024-02-20 16:30:18 +0530

    Update 06_ComparisonOfDataTypes.js

--- 06_ComparisonOfDataTypes.js
+++ 06_ComparisonOfDataTypes.js
@@ -5,19 +5,19 @@
 // console.log(2 != 1);
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+// console.log("2" > 1); // true
+// console.log("02" > 1); // true
 
 // The reason is that an equality check == and comparisons > < >= <= works differently
 // comparisons convert null to a number treating as 0 thats why (3) null >=0 is true and (1) null > 0 is false
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+console.log(undefined == 0); // false
+console.log(undefined > 0); // false
+console.log(undefined < 0); // false
 
 // === 
 
