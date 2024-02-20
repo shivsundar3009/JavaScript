77043b3d7ae1677eb799ab779a2a5f4aee7a8ab6
shivsundar wakle <126707206+shivsundar3009@users.noreply.github.com>
2024-02-20 16:17:08 +0530

    Create 05_prefix_postfix.js

---
+++ 05_prefix_postfix.js
@@ -0,0 +1,19 @@
+// let x = 5
+
+// Postfoxit operator will return the value without any operations
+
+// let y = x++
+
+// let z = y
+
+// console.log(z);
+
+let x = 5
+
+// Prefix operator will first do operation than return the value
+
+let y = ++x
+
+let z = y
+
+console.log(z);
